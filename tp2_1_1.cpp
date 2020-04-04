2fa2da4d76a98fdb157125ed8f7e7fad6536a598
FNMariani <facundonicolasm@gmail.com>
2020-04-04 16:21:22 -0300

    Create file tp2_1_1

---
+++ tp2_1_1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define N 4
+#define M 5
+
+int f,c;
+double mt[N][M];
+
+int main()
+{
+    for(f = 0;f<N; f++)
+    { 
+        for(c = 0;c<N; c++)
+        {
+            printf("%lf   ", mt[f][c]);
+        }
+        printf("\n");
+    }
+    getchar();
+}
